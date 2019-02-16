b353cacb8a37d08e596cccd11778befed23c111b
ouellec <ouellec@wwu.edu>
2019-02-16 14:31:40 -0800

    added classes bulk json

---
+++ webscraper/bulkload.js
@@ -0,0 +1,18 @@
+const classes = require("./classes");
+const fs = require('fs');
+
+
+const index = "class";
+const type = "classlist";
+
+
+
+var stream = fs.createWriteStream("classes_bulk.json", {flags:'a'});
+
+for(let i = 0; i< classes.length; i++ ){
+    const place = {"index":{"_index": index, "_type": type, "_id": i}}
+    const data = classes[i];
+    var placejson = JSON.stringify(place);
+    var datajson = JSON.stringify(data);
+    stream.write(placejson + "\n" + datajson + "\n");
+}
