a8e14f86e6759889c16f24bc6c9b073973aa3c04
Haley Beavers <beaverh@wwu.edu>
2019-02-16 12:22:55 -0800

    creating search bar

---
+++ client/classer-finder/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Column, Row} from 'simple-flexbox';
+import search_icon from './icons/search3.png';
+
+class App extends Component {
+  render() {
+    return (
+      <Column vertical = 'center' horizontal = 'center'>
+        <Row horizontal = 'center' vertical = 'center'>
+          <div>
+              <Column horizontal = 'center'>
+                <h1>WWU ClasserFinder</h1>
+              </Column>
+            <div className='flex-search'>
+              <Row horizontal = 'center' vertical = 'center'>
+                <input className='search-bar' type='text'></input>
+              </Row>
+              <img className='search-icon' src={search_icon} alt='magnifying glass'></img>
+            </div>
+          </div>
+        </Row>
+      </Column>
+      
+    );
+  }
+}
+
+export default App;
