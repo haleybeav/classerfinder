298dd4dfa8795e12d8148d9be2eaeac015deed12
Haley Beavers <beaverh@wwu.edu>
2019-02-16 12:37:18 -0800

    yoooo

--- client/classer-finder/src/App.js
+++ client/classer-finder/src/App.js
@@ -4,6 +4,14 @@ import {Column, Row} from 'simple-flexbox';
 import search_icon from './icons/search3.png';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: ''
+    }
+  }
+
   render() {
     return (
       <Column vertical = 'center' horizontal = 'center'>
@@ -14,14 +22,13 @@ class App extends Component {
               </Column>
             <div className='flex-search'>
               <Row horizontal = 'center' vertical = 'center'>
-                <input className='search-bar' type='text'></input>
+                <input className='search-bar' type='text' onChange={ (e) => this.setState({ query: e.target.value })}></input>
               </Row>
               <img className='search-icon' src={search_icon} alt='magnifying glass'></img>
             </div>
           </div>
         </Row>
       </Column>
-      
     );
   }
 }
