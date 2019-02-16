fc8b8f5225477622c7ef635722de502e79c9a7fc
ouellec <ouellec@wwu.edu>
2019-02-16 14:50:11 -0800

    building server.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const  express = require('express');
 const bodyParser = require("body-parser");
-
+const axios = require('axios');
 
 
 const app = express();
@@ -11,12 +11,32 @@ const port = 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
 
 
+const response = await client.search({
+    index: 'class',
+    type: 'classlist',
+    body: {
+        query: {
+            match: {
+            body: 'elasticsearch'
+            }
+        }
+    }
+});
+
+for (const tweet of response.hits.hits) {
+    console.log('tweet:', tweet);
+}
 
 app.get("/classes/:term", (req, res)=>{
 
 });
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}! WOOOO HOOO`));
+//app.listen(port, () => console.log(`Example app listening on port ${port}! WOOOO HOOO`));
