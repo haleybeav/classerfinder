8fccf357310ab39e585dc60ebcfbf0c0db1285f5
ouellec <ouellec@wwu.edu>
2019-02-17 00:08:39 -0800

    styling.

--- client/classer-finder/src/Schedule/Schedule.js
+++ client/classer-finder/src/Schedule/Schedule.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import './Schedule.css'
 import Day from './Day/Day';
+import {CLASSCOLOR} from './Colors';
 
 const Schedule = ({courses}) => {
+
+    let i = -1;
+    let classlist =  courses.map((course)=>{
+        i = i + 1;
+        let style={  "backgroundColor": CLASSCOLOR[i], }
+        return (    <div> <div className="dot" style={style}></div> { course.title} </div>)
+    });
     return (
-        <div> 
+        
             <div className='schedule-bar'>
+                <div className="times"> 
+                    <div>8am</div>                     
+                    <div>12pm</div>                    
+                    <div>6pm</div> 
+                </div>
                 <Day day="M" data={courses} />
                 <Day day="T" data={courses} />
                 <Day day="W" data={courses} />
                 <Day day="R" data={courses} />
                 <Day day="F" data={courses} />            
+                <div className="vLine" />
+                <div className="class-list">
+                    <h3> Selected Classes </h3>
+                        {classlist}
+                </div>
             </div>
-            <div>
-                O_O
-
-            </div>
-        </div>        
+                    
     );
 }
 
