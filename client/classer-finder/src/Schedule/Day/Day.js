20d9a7c369844dda688aa218c32a7633c3358c67
ouellec <ouellec@wwu.edu>
2019-02-16 21:38:11 -0800

    build out calendar.

---
+++ client/classer-finder/src/Schedule/Day/Day.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './Day.css'
+
+const DAYHEIGHT = 300;
+const STARTTIME = 8;
+const ENDTIME = 18;
+// 8 to 18
+
+
+const DAYNAME = {
+    "M": "Monday",
+    "T": "Tuesday",
+    "W": "Wednesday",
+    "R": "Thursday",
+    "F": "Friday"
+}
+
+const CLASSCOLOR = [
+    "#FFB800",
+    "#6EEB83",    
+    "#0CE0E8",
+    "#A20CE8",
+    "#E80CAA"
+]
+
+
+const getIntFromString = (str)=>{
+    let time = str.split(":");
+    let intTime = parseInt(time[0]);
+    intTime = intTime + parseInt(time[1])/60;
+    return intTime;
+}
+
+const getPixelsStartFromTime = (time)=>{
+    return (time - STARTTIME) * (DAYHEIGHT /(ENDTIME - STARTTIME));    
+}
+
+
+const getPixelsSizeFromTime = (sTime, eTime)=>{
+    return (eTime - sTime) * (DAYHEIGHT /(ENDTIME - STARTTIME));
+}
+
+const Day = ({day, data}) => {
+    var classBlocks = [];
+    for (var i = 0; i < data.length; i++) {
+
+        if (data[i].time[day]){
+
+            let sTime = getIntFromString(data[i].time[day][0]);
+            let eTime = getIntFromString(data[i].time[day][1]);            
+
+            let classStyle = {
+                "top": getPixelsStartFromTime(sTime),
+                "height": getPixelsSizeFromTime(sTime, eTime),
+                "backgroundColor": CLASSCOLOR[i],
+                "width": "100%",
+                "position": "absolute",
+                "borderRadius": "4px",
+                "margin": "2px"                
+            };
+    
+            classBlocks.push(<div className='class' style={classStyle} key={i}></div>);
+        }        
+    }
+    return (
+        <div className='day-bar'>
+            {DAYNAME[day]}
+
+            {classBlocks}
+        </div>
+    );
+}
+
+export default Day;
