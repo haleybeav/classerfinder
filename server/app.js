495681ecc33097204515c4737a989dc0fe056189
ouellec <ouellec@wwu.edu>
2019-02-16 16:26:02 -0800

    removed source

--- server/app.js
+++ server/app.js
@@ -27,12 +27,12 @@ app.get("/classes/:term", async (req, res)=>{
             "source" : {
                 "from" : 0, 
                 "size" : 20,
+                "explain": true,
                 "query": {
                     "query_string" : {
                         "query": "{{my_query}}"
                     }
-                },
-                "_source": ["dept", "course", "title", "crn", "instructor", "location"]
+                }                
             },
             "params" : {       
                 "my_query" : req.params.term
